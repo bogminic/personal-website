108db0bcd283537cce860d2674c9869bc2fc5a4e
Bogdan <bmnicolae@gmail.com>
2020-05-05 10:59:55 +0300

    Update email for form

--- src/contact-form.php
+++ src/contact-form.php
@@ -1,5 +1,5 @@
 <?php
-$toEmail = "bogdan@bmn.com.ro";
+$toEmail = "bmnicolae@gmail.com";
 $mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["userEmail"] .">\r\n";
 if(mail($toEmail, $_POST["subject"], $_POST["content"], $mailHeaders)) {
 print "Contact Mail Sent. Thank you!";
